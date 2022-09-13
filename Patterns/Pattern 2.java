5e3c9e10653e7e24e16cad750efe95ecf880b4f7
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-09-14 00:01:56 +0530

    Create Pattern 2.java

---
+++ Patterns/Pattern 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+public static void main(String[] args){
+Scanner scn=new Scanner(System.in);
+  int n=scn.nextInt();
+  int nstars=n;
+  for(int i=1;i>=nstars;i++){
+  System.out.println("*\t");
+  }
+}
+}
