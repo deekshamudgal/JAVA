38ad0169507fa169fa54a2311a5c86bff6045fc6
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-09-25 17:25:55 +0530

    Create Introduction to Arraylists.java

---
+++ String, String Builder and Array List/Introduction to Arraylists.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    ArrayList< Integer> al = new ArrayList< >(); //declares an integer arraylist
+    Arraylist< String> a2 = new Arraylist < >(); //declares an arraylist of string
+
+
+  }
+
+}
