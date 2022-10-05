2856a64db7c2e563e56ebce3a3332fd5418f58ad
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-10-05 15:22:22 +0530

    Create Size Generic Tree.java

---
+++ Binary Tree/Size Generic Tree.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Size_Generic_Tree{
+
+  public static class Node{
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static int size(Node node){
+    // write your code here
+    int size=0;
+    for(Node child:node.children){
+        int cs=size(child);
+        size=size+cs;
+    }
+    size=size+1;
+    return size;
+    
+  }
+
+}
+
+
+  public static void main(String[] args){
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+  }
+  
