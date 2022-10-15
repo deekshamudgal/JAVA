317187ebbd35a1fddf974944f21943d9cfcfb184
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-10-15 07:43:43 +0530

    Create Introduction To Generic Tree.java

---
+++ Linked LIsts/Generic Tree/Introduction To Generic Tree.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+import java.io.*;
+public class Main
+{
+  public class Node
+  {
+    int data;  //for storing the data
+    ArrayList< Node> children = new ArrayList< >(); //for storing the child nodes
+  }
+  public static void main(String[] args)
+  {
+    Node root; //unique node for a tree
+  }
+}
