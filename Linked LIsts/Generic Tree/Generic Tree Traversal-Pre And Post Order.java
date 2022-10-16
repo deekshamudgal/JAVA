99ea1b64d8566f7ec9b5955ae4228ab7385934f4
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-10-16 08:06:20 +0530

    Create Generic Tree Traversal-Pre And Post Order.java

---
+++ Linked LIsts/Generic Tree/Generic Tree Traversal-Pre And Post Order.java
@@ -0,0 +1,19 @@
+public static void traversals(Node node) {
+
+  //Euler's Left/ Node Pre/ Into the recursion
+  System.out.println("Node Pre " + node.data);
+
+
+  for (Node child : node.children)
+  {
+    //Edge Pre
+    System.out.println("Edge Pre " + node.data + "--" + child.data);
+    traversals(child);
+    System.out.println("Edge Post " + node.data + "--" + child.data);
+    //Edge Post
+  }
+  System.out.println("Node Post " + node.data);
+  //Euler's right/Node Post/on the way out of recursion
+
+
+}
