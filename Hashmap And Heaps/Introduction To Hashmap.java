6376c06d28854327a667447cd0d72c5c42fdd562
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-10-22 10:14:35 +0530

    Create Introduction To Hashmap.java

---
+++ Hashmap And Heaps/Introduction To Hashmap.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args)
+  {
+    HashMap< String, Integer> hm = new HashMap< >();
+    System.out.println(hm);
+  }
+}
