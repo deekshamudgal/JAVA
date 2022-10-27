51ba8c9bb8a01154407c515cee91c9c25d5eef80
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-10-27 14:33:56 +0530

    Create Abbreviation Using Backtracking.java

---
+++ Recursion And Backtracking/Abbreviation Using Backtracking.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Main {
+
+  public static void solution(String str, String asf, int count, int pos) {
+    if (pos == str.length()) {
+      if (count > 0) {
+        asf += count;
+      }
+      System.out.println(asf);
+      return;
+    }
+    solution(str, asf + (count > 0 ? count : "" ) + str.charAt(pos), 0, pos + 1); //yes call -> including curr char
+    solution(str, asf, count + 1, pos + 1); //not including curr char -> just converting letters to a number
+  }
+
+}
