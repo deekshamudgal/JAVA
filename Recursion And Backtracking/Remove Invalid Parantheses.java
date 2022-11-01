365882ce08b3d7789bab83169bd8c5bea5d508d4
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-11-01 21:53:44 +0530

    Update Remove Invalid Parantheses.java

--- Recursion And Backtracking/Remove Invalid Parantheses.java
+++ Recursion And Backtracking/Remove Invalid Parantheses.java
@@ -1,19 +1,68 @@
-public static int getMin(String str) {
-  Stack st = new Stack<>();
+import java.io.*;
 
-  for (int i = 0; i < str.length(); i++) {
-    char ch = str.charAt(i);
-    if (ch == '(' ) {
-      st.push(ch);
+import java.util.*;
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    String str = scn.next();
+    solution(str, getMin(str), new HashSet<>());
+  }
+
+  public static void solution(String str, int minRemoval, HashSet ans) {
+    if (IsValid(str) == true) {
+      if (!ans.contains(str)) {
+        System.out.println(str);
+        ans.add(str);
+      }
+      return;
     }
-    else
-    { if (st.size() == 0 || st.peek() == ')' ) {
-        st.push(ch);
-      }
-      else if (st.peek() == '(' ) {
-        st.pop();
-      }
+
+    if (minRemoval == 0) {
+      return;
+    }
+
+    for (int i = 0; i < str.length(); i++) {
+      String left = str.substring(0, i);
+      String
+      right = str.substring(i + 1);
+      solution(left + right, minRemoval - 1, ans);
     }
   }
-  return st.size();
+  public static int getMin(String str) {
+    Stack st = new Stack<>();
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if (ch == '(' )
+      {
+        st.push(ch);
+      }
+      else {
+        if (st.size() == 0 || st.peek() == ')' ) {
+          st.push(ch);
+        }
+        else if (st.peek() == '(' ) {
+          st.pop();
+        }
+      }
+    }
+    return
+      st.size();
+  } private static boolean IsValid(String str) {
+    int count = 0;
+    for (int i = 0 ; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if (ch == '('
+         ) {
+        count++;
+      }
+      else if (ch == ')' ) {
+        count--;
+        if (count < 0) {
+          return false;
+        }
+      }
+    }
+    return count == 0;
+  }
 }
