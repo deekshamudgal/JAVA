e764ceb34719a185bfa0b2b151dafa1025706af0
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-11-23 10:58:08 +0530

    Create Abbreviation 1 - Using Bits.java

---
+++ Bits Manipulation/Abbreviation 1 - Using Bits.java
@@ -0,0 +1,30 @@
+public static void solve(String str) {
+  for (int i = 0;
+       i < (1 << str.length()); i++) { //1
+    StringBuilder sb = new StringBuilder();
+    int count = 0;
+
+    for (int j = 0; j < str.length(); j++) { //2
+      char ch = str.charAt(j);
+      int b = str.length() - 1 - j;
+
+      if ((i & (1 << b)) == 0) { //3
+        if (count == 0) { //4
+          sb.append(ch);
+        } else { //5
+          sb.append(count);
+          sb.append(ch);
+          count = 0;
+        }
+      } else { //6
+        count++;
+      }
+    }
+
+    if (count > 0) { //7
+      sb.append(count);
+    }
+
+    System.out.println(sb); //8
+  }
+}
